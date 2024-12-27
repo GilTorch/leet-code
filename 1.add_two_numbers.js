b67900a48e25a451ecf1b4a05d205e0e4ec5c715
GilTorch <infotorch2014@gmail.com>
2024-12-27 04:21:16 -0400

    solve add_two_numbers

---
+++ 1.add_two_numbers.js
@@ -0,0 +1,12 @@
+// Exercise: https://leetcode.com/problems/two-sum/
+var twoSum = function(nums, target) {
+    
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(i !== j && nums[i]+nums[j]===target){
+                return [i,j];
+            }
+        }
+    }
+
+};
